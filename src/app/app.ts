94bdd7bd0c592924e50ddbe71437d680febc6dad
Rinaldo Alves <r.medeiros1206@gmail.com>
2026-01-03 23:52:11 -0300

    secundary-button / itens da lista de certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar'
 import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, PrimaryButton],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
