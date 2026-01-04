2950a847460fa3dd12fef358c2c8e640cdaaf802
Rinaldo Alves <r.medeiros1206@gmail.com>
2026-01-04 12:22:17 -0300

    baseUi / page certificado-form / page certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,15 @@ import { Navbar } from './_components/navbar/navbar'
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
+import { CertificadoForm } from "./pages/certificado-form/certificado-form";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
