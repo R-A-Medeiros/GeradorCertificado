9a4a6540fa06608e61fbaa0ede0a77cd5b3bfba2
Rinaldo Alves <r.medeiros1206@gmail.com>
2026-01-03 23:05:03 -0300

    Primeiro commit

---
+++ src/app/_components/navbar/navbar.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar implements OnInit {
+  ngOnInit(): void {
+    this.mensagem();
+  }
+
+
+  mensagem() {
+    console.log('navbar inicializou (dentro de um método)');
+  }
+}
---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './_components/navbar/navbar'
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Navbar, PrimaryButton],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('gerador-certificado');
+
+  exibeNavBar: boolean = true;
+}
